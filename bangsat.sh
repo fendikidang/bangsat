8f8f88e6aa5815f1bce8fb262a02b2571daedf8d
fendikidang <50740208+fendikidang@users.noreply.github.com>
2019-06-10 08:17:36 +0700

    Update bangsat.sh

--- bangsat.sh
+++ bangsat.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
@@ -7,8 +8,6 @@ me='\033[31;1m' #merah
 pu='\033[37;1m' #purple
 ku='\033[33;1m' #kuning
 echo
-echo
-python2 meizu.py
 echo
 clear
 echo
